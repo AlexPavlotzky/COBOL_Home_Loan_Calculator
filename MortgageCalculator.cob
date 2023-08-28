f72e455e37fbfc3b7c59c41d4993392eac240257
AlexPavlotzky <alexpavlotzky@gmail.com>
2023-08-28 23:16:49 +0000

    refactorización

--- MortgageCalculator.cob
+++ MortgageCalculator.cob
@@ -1,31 +1,32 @@
-           IDENTIFICATION DIVISION.
-           PROGRAM-ID. MortgageCalculator.
-           AUTHOR. Alexander Pavlotzky.
-           DATE-WRITTEN. 27/08/2023.
-           DATE-COMPILED. 28/08/2023.
-           DATA DIVISION.
-           WORKING-STORAGE SECTION.
-           01 LoanAmount           PIC 9(8)V99.
-           01 InterestRate         PIC 9(3)V99.
-           01 LoanTermYears        PIC 9(3).
-           01 MonthlyPayment       PIC 9(8)V99.
-           01 TotalInterestPaid    PIC 9(8)V99.
-           PROCEDURE DIVISION.
-              DISPLAY "Mortgage Calculator".
-              DISPLAY "Enter Loan Amount: ".
-              ACCEPT LoanAmount.
-              DISPLAY "Enter Interest Rate: ".
-              ACCEPT InterestRate.
-              DISPLAY "Enter Loan Term (in years): ".
-              ACCEPT LoanTermYears.
-                
-              COMPUTE InterestRate = InterestRate / 100.
-              COMPUTE MonthlyPayment = (LoanAmount * (InterestRate / 12)) / (1 - (1 + InterestRate / 12) ** (-LoanTermYears * 12)).
-              COMPUTE TotalInterestPaid = (MonthlyPayment * LoanTermYears * 12) - LoanAmount.
-                
-              DISPLAY "Monthly Payment: $" MonthlyPayment.
-              DISPLAY "Total Interest Paid: $" TotalInterestPaid.
-                
-              STOP RUN.
-           END PROGRAM MortgageCalculator.
-      
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MORTGAGECALCULATOR.
+       AUTHOR. Alexander Pavlotzky.
+       DATE-WRITTEN. 27/08/2023.
+       DATE-COMPILED. 28/08/2023.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 LOANAMOUNT         PIC 9(8)V99.
+       01 INTERESTRATE       PIC 9(3)V99.
+       01 LOANTERMYEARS      PIC 9(3).
+       01 MONTHLYPAYMENT     PIC 9(8)V99.
+       01 TOTALINTERESTPAID  PIC 9(8)V99.
+       PROCEDURE DIVISION.
+           DISPLAY "Mortgage Calculator".
+           DISPLAY "Enter Loan Amount: ".
+           ACCEPT LOANAMOUNT.
+           DISPLAY "Enter Interest Rate: ".
+           ACCEPT INTERESTRATE.
+           DISPLAY "Enter Loan Term (in years): ".
+           ACCEPT LOANTERMYEARS.
+            
+           COMPUTE INTERESTRATE = INTERESTRATE / 100.
+           COMPUTE MONTHLYPAYMENT =(LOANAMOUNT *(INTERESTRATE / 12))
+              /(1 - (1 + INTERESTRATE / 12) **(- LOANTERMYEARS * 12)).
+           COMPUTE TOTALINTERESTPAID =(MONTHLYPAYMENT
+              * LOANTERMYEARS * 12) - LOANAMOUNT.
+            
+           DISPLAY "Monthly Payment: $" MONTHLYPAYMENT.
+           DISPLAY "Total Interest Paid: $" TOTALINTERESTPAID.
+            
+           STOP RUN.
+       END PROGRAM MORTGAGECALCULATOR.
